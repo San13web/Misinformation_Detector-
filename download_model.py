95bbefe671883bbc5b212f18a60f6ec4b95fdb3d
Saniya <saniyagudekar37763@gmail.com>
2025-02-28 02:08:15 +0530

    Removed duplicate files from root directory

--- download_model.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-def download_model():
-    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-    print("Model downloaded successfully!")
-
-if __name__ == "__main__":
-    download_model()
